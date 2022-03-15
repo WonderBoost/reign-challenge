07408a9b4b6b8178f68fbb002f5053c5bc6e2285
wonderboost <josegsv28@gmail.com>
2022-03-15 15:17:29 -0400

    initialize project

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css'
+
+function Nav() {
+  return (
+    <div className='rectangle'>
+        <span className='hacker-news'>HACKER NEWS</span>
+    </div>
+  )
+}
+
+export default Nav
